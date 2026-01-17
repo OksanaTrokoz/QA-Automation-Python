697b38faf19ee2b8e0e0647629b9cd9a87e6511a
Oksana Trokoz <oksana.trokoz.it@gmail.com>
2026-01-17 19:17:33 +0100

    HW02. Виконані зміни

---
+++ lesson_01/homework_01.py
@@ -0,0 +1,66 @@
+# task 01
+print("Hello", end=" ")
+print("world!")
+
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print(f"{hello} {world}!")
+
+# task 03  == Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter)
+
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+banana = apples * 4
+print(banana)
+
+# task 05 == виправте назви змінних
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+
+# task 06 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+perimetery = storona_1+ storona_2 + storona_3+ storona_4
+print(perimetery)
+
+# task 07
+
+apple_tree = 4
+pear_tree = apple_tree + 5
+plum_tree = apple_tree - 2
+print(apple_tree + pear_tree + plum_tree)
+
+# task 08
+
+befor_l = 0 + 5
+after_l = befor_l - 10
+evening = after_l + 4
+print(evening)
+
+# task 09
+
+all_boys = 24
+all_girls = all_boys // 2
+today_boys = all_boys - 1
+today_girls = all_girls - 2
+print(today_boys+today_girls)
+
+
+# task 10
+
+book_1 = 8
+book_2 = book_1 + 2
+book_3 = (book_1 + book_2) / 2
+book_all = book_1 + book_2 + book_3
+print(book_all)
+
+
+
+
