67df9e230bdd3ff95fa3dd39af5131dcf996c9cb
Oksana Trokoz <oksana.trokoz.it@gmail.com>
2026-02-02 13:36:26 +0100

    HW06.2

---
+++ lesson_06/HW_6.2.py
@@ -0,0 +1,9 @@
+while True:
+   word_input = input('Please word with letters: h and H:')
+
+   if 'h' in word_input.lower():
+        print('Thank you')
+        break
+   else:
+        print('Try again')
+
